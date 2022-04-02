3804ddda897861061df006d2c09ada73bde7b0d8
lorriman <48987+lorriman@users.noreply.github.com>
2022-04-02 23:28:11 +0100

    preliminary transition code in cupertino_tab_view_router.dart

--- lib/routing/cupertino_tab_view_router.dart
+++ lib/routing/cupertino_tab_view_router.dart
@@ -3,6 +3,26 @@ import 'package:flutter/cupertino.dart';
 /// Copyright Andrea Bozito, with modifications.
 /// Notable additions and classes by Greg Lorriman as noted.
 
+
+class CupertinoPageRouteAnimated extends CupertinoPageRoute {
+  CupertinoPageRouteAnimated({
+    required WidgetBuilder builder,
+    String? title,
+    RouteSettings? settings,
+    bool maintainState=true,
+    bool fullscreenDialog=false,
+  })
+      : super(builder: builder, title: title, settings: settings, maintainState : maintainState, fullscreenDialog: fullscreenDialog);
+
+
+  // OPTIONAL IF YOU WISH TO HAVE SOME EXTRA ANIMATION WHILE ROUTING
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return new FadeTransition(opacity: animation, child: Text('1st placeholder: see cupertino_tab_view_router '));
+  }
+}
+
 class CupertinoTabViewRoutes {
   static const checklistItemEntriesPage = '/checklistItem-entries-page';
 }
@@ -11,7 +31,7 @@ class CupertinoTabViewRouter {
   static Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case CupertinoTabViewRoutes.checklistItemEntriesPage:
-        return CupertinoPageRoute(
+        return CupertinoPageRouteAnimated(
           builder: (_) => Center(
               child: Text(
                   'placeholder, see cupertino_tab_view_router.dart')), //ChecklistItemEntriesPage(checklistItem: checklistItem),
